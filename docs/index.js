0d450524f4dc11bb4ae4e9b9bd0d9e0a40b8cbbd
jmvilarinho <jmvilarinho@gmail.com>
2024-07-21 10:38:18 +0200

    webcam playas

--- docs/index.js
+++ docs/index.js
@@ -53,14 +53,6 @@ async function showVideo(url, videoid) {
 
 // --------------------------------------------------------------------------------------------------
 
-function getMareas(id, element) {
-	url = "https://ideihm.covam.es/api-ihm/getmarea?request=gettide&id=" + id + "&format=json"
-	console.log('Mareas: ' + url)
-	fetch(url)
-		.then(response => response.json())
-		.then(data => createList(data, element));
-}
-
 function padTo2Digits(num) {
 	return num.toString().padStart(2, '0');
 }
@@ -72,11 +64,21 @@ function getLocalTime(time) {
 	return padTo2Digits(date.getHours()) + ':' + padTo2Digits(date.getMinutes());
 }
 
+function getMareas(id, element='') {
+	url = "https://ideihm.covam.es/api-ihm/getmarea?request=gettide&id=" + id + "&format=json"
+	console.log('Mareas: ' + url)
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			return createList(data, element);
+		});
+}
+
 function createList(data, element) {
 	const mainDiv = document.getElementById(element);
 
 	var ubicacion = data["mareas"]["puerto"];
-	var fecha=getFechaES( data["mareas"]["fecha"]);
+	var fecha = getFechaES(data["mareas"]["fecha"]);
 	var datos = data['mareas']['datos']['marea'];
 	var mareas = ''
 		+ datos[0]['tipo'] + ": " + getLocalTime(datos[0]['hora'])
@@ -87,23 +89,28 @@ function createList(data, element) {
 		+ ", "
 		+ datos[3]['tipo'] + ": " + getLocalTime(datos[3]['hora'])
 
-	const keyDiv = document.createElement('div');
-	keyDiv.innerHTML = `Mareas en ${ubicacion} (${fecha})<br> ${mareas}`;
-	mainDiv.appendChild(keyDiv);
+	if ( element != ''){
+		const keyDiv = document.createElement('div');
+		keyDiv.innerHTML = `Mareas en ${ubicacion} (${fecha})<br> ${mareas}`;
+		mainDiv.appendChild(keyDiv);
+	}
+	return mareas;
 }
 
 // --------------------------------------------------------------------------------------------------
 
 var apikey = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqbXZpbGFyaW5ob0BnbWFpbC5jb20iLCJqdGkiOiJhZTdiYTgwOS1iOTQ3LTQxM2YtYmRmYy03ODEzZjMxOGM5ZDkiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTcyMTQ4NDg2MiwidXNlcklkIjoiYWU3YmE4MDktYjk0Ny00MTNmLWJkZmMtNzgxM2YzMThjOWQ5Iiwicm9sZSI6IiJ9.7kqIc3ErJmp9MtGELp9C8SDUkZ-a9bAX2LeRw_aysRg';
 
-function getPrevision(id, element) {
+function getPrevision(id, element, idmareas=0) {
 	const ms = Date.now();
-	fetch('https://opendata.aemet.es/opendata/api/prediccion/especifica/playa/' + id + '/?api_key=' + apikey+"&dummy="+ms)
+	const url = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/playa/' + id + '/?api_key=' + apikey + "&dummy=" + ms
+	console.log('Get prevision  playa: ' + url)
+	fetch(url)
 		.then(response => response.json())
-		.then(data => getPrevisionDatos(data, element));
+		.then(data => getPrevisionDatos(data, element, idmareas));
 }
 
-function getPrevisionDatos(data, element) {
+function getPrevisionDatos(data, element, idmareas) {
 	if (data['estado'] == 200) {
 
 		console.log('Get prevision: ' + data['datos'])
@@ -117,7 +124,7 @@ function getPrevisionDatos(data, element) {
 			.then(function (buffer) {
 				const decoder = new TextDecoder('iso-8859-1');
 				const text = decoder.decode(buffer);
-				createPrevision(JSON.parse(text), element);
+				createPrevision(JSON.parse(text), element, idmareas);
 			});
 	}
 }
@@ -132,7 +139,7 @@ function getFechaES(fecha) {
 	return dt.toLocaleDateString("es-ES", options)
 }
 
-function createPrevision(data, element) {
+function createPrevision(data, element, idmareas) {
 	var tabla = '<table class="center">';
 	var datos = data[0]["prediccion"]["dia"][0];
 
@@ -147,7 +154,7 @@ function createPrevision(data, element) {
 		+ "</tr><tr>"
 		+ "<th colspan=2>Temp. Auga</th><td>" + datos["tAgua"]["valor1"] + "&deg;</td>"
 		+ "</tr><tr>"
-		+ "<th rowspan=4>Mañá</th>"
+		+ '<th rowspan=4>Mañá<br><img src="img/' + datos["estadoCielo"]["f1"] + '.png" height="50px"></th>'
 		+ "<tr>"
 		+ "<th>Ceo</th><td style='text-align: left;'>" + datos["estadoCielo"]["descripcion1"] + "</td>"
 		+ "<tr>"
@@ -155,21 +162,25 @@ function createPrevision(data, element) {
 		+ "<tr>"
 		+ "<th>Oleaxe</th><td style='text-align: left;'>" + datos["oleaje"]["descripcion1"] + "</td>"
 		+ "</tr><tr>"
-		+ "<th rowspan=4>Tarde</th>"
-		+ "<tr>"
+		+ '<th rowspan=4>Tarde<br><img src="img/' + datos["estadoCielo"]["f2"] + '.png" height="50px"></th>'		+ "<tr>"
 		+ "<th>Ceo</th><td style='text-align: left;'>" + datos["estadoCielo"]["descripcion2"] + "</td>"
 		+ "<tr>"
 		+ "<th>Vento</th><td style='text-align: left;'>" + datos["viento"]["descripcion2"] + "</td>"
 		+ "<tr>"
 		+ "<th>Oleaxe</th><td style='text-align: left;'>" + datos["oleaje"]["descripcion2"] + "</td>"
 		+ "</tr><tr>";
+
+	// if ( idmareas > 0){
+	// 	tabla += '<tr><td colspan=3>'+getMareas(idmareas)+'</td></tr>';
+	// }
+
 	tabla += "</table>";
 
 	var dt = new Date(data[0]["elaborado"]);
 	var options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
 	document.getElementById("data_prevision").innerHTML = "<p style='font-size:12px;'>["
-	+"<a href='http://www.aemet.es' target='copyright'>"
-	+"Previsión meteorolóxica por AEMET: "
+		+ "<a href='http://www.aemet.es' target='copyright'>"
+		+ "Previsión meteorolóxica por AEMET: "
 		+ dt.toLocaleDateString("es-ES", options)
 		+ "</a>]</p>";
 
